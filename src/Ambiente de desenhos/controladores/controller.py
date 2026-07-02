097c52adf794ef777c9394f72b312ad977baa4fd
Guilherme Santana de Matos <guisantanamatos16@gmail.com>
2026-07-01 22:45:46 -0300

    Entrega final completa do MVC

--- src/Ambiente de desenhos/controladores/controller.py
+++ src/Ambiente de desenhos/controladores/controller.py
@@ -1,55 +1,54 @@
-from modelos.classes import *
-from view import *
+from modelos.classes import Retangulo, Oval, Circulo, Linha, Rabisco
+from view.view import desenhar_figura_na_tela  # <- Importe a função que criamos para desenhar na View
 
 class Controlador:
     def __init__(self, view, desenho):
         self.view = view
         self.desenho = desenho
-        self.figuras = []                    # GUARDA AS FIGURAS PRONTAS
-        self.figura_preview = None           # GUARDA O RASCUNHO QUE APARECE NA HORA EM QUE VOCÊ DESENHA
-        self.pontos_poligono = []            # GUARDA AS COORDENADAS PARA O POLIGONO 
-        self.linha_poligono = None           # GUARDA O RASCUNHO DO POLIGONO 
-        self.linhas_poligono = []            # LINHAS PARA O RASCUNHO DO POLIGONO
 
         self.x_ini = 0
         self.y_ini = 0
 
-        # Vincular eventos do mouse aos métodos correspondentes
+        # RASCUNHO DE LINHA DO POLÍGONO
+        self.linha_poligono_temp = None           
+        self.linhas_poligono_fixas_temp = []      
+
+        # EVENTOS DO MOUSE
         self.view.canvas.bind("<Button-1>", self.iniciar_desenho)
         self.view.canvas.bind("<B1-Motion>", self.mover_desenho)
-        self.view.canvas.bind("<ButtonRelease-1>", self.terminarDesenho)
-        self.view.canvas.bind("<Double-Button-1>", terminarPoligono)
-        self.view.canvas.bind("<Motion>", moverPoligono)
+        self.view.canvas.bind("<ButtonRelease-1>", self.terminar_desenho)
+        self.view.canvas.bind("<Double-Button-1>", self.terminar_poligono)
+        self.view.canvas.bind("<Motion>", self.mover_poligono)
 
     def iniciar_desenho(self, event):
-        global x_ini, y_ini, figura_preview, pontos_poligono, linhas_poligono, linha_poligono
         self.x_ini = event.x
         self.y_ini = event.y
 
-        # RABISCO :
         forma_atual = self.view.obter_forma()
         cor_borda = self.view.obter_cor_borda()
-        cor_preenchimento = self.view.obter_cor_preench()
 
         if forma_atual == "Rabisco":
-            figura_preview = Rabisco(x_ini, y_ini, cor_borda)
+            self.desenho.figura_preview = Rabisco(self.x_ini, self.y_ini, cor_borda)
     
-        elif forma_atual == "Polígono":      #SE FOR POLIGONO, APENAS SALVA OS PONTOS NA LISTA DE PONTOS
-            pontos_poligono.append(x_ini)
-            pontos_poligono.append(y_ini)
+        elif forma_atual == "Polígono":
+            # GUARDA O PONTO CLICADO PELO MODELO LÁ DO ARQUIVO DESENHO.PY
+            self.desenho.adicionar_ponto_poligono(self.x_ini, self.y_ini)
 
-            if len(pontos_poligono) >= 4:
-                linha_fixa = self.view.canvas.create_line(pontos_poligono[-4], pontos_poligono[-3], pontos_poligono[-2], pontos_poligono[-1])
-                linhas_poligono.append(linha_fixa)
+            # SE JÁ TEM DOIS OU MAIS PONTOS CLICADOS
+            if len(self.desenho.pontos_poligono) >= 4:
+                linha_fixa = self.view.canvas.create_line(
+                    self.desenho.pontos_poligono[-4], self.desenho.pontos_poligono[-3], 
+                    self.desenho.pontos_poligono[-2], self.desenho.pontos_poligono[-1]
+                )
+                self.linhas_poligono_fixas_temp.append(linha_fixa)
         
-            linha_poligono = None
-            return
+            self.linha_poligono_temp = None
     
     def mover_desenho(self, event):
         # RASCUNHO VISTO NA HORA 
-        global figura_preview
         if self.view.obter_forma() == "Polígono":
             return
+            
         x_atual = event.x
         y_atual = event.y
         
@@ -58,111 +57,84 @@ class Controlador:
         cor_borda = self.view.obter_cor_borda()
         cor_preenchimento = self.view.obter_cor_preench()
         
+
         # RABISCO SE ADICIONA APENAS O PONTO AO OBJETO
         if forma_atual == "Rabisco":
-            if isinstance(figura_preview, Rabisco):
-                figura_preview.adicionar_pontos(x_atual, y_atual)
+            if isinstance(self.desenho.figura_preview, Rabisco):
+                self.desenho.figura_preview.adicionar_pontos(x_atual, y_atual)
         else: 
             if forma_atual == "Retângulo":
-                figura_preview = Retangulo(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
+                self.desenho.figura_preview = Retangulo(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
             elif forma_atual == "Oval":
-                figura_preview = Oval(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
+                self.desenho.figura_preview = Oval(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
             elif forma_atual == "Círculo":
-                figura_preview = Circulo(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
+                self.desenho.figura_preview = Circulo(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda, cor_preenchimento)
             elif forma_atual == "Linha":
-                figura_preview = Linha(x_ini, y_ini, x_atual, y_atual, cor_borda)
+                self.desenho.figura_preview = Linha(self.x_ini, self.y_ini, x_atual, y_atual, cor_borda)
         
         self.desenhar_tudo()
 
     def mover_poligono(self, event):
         #RASCUNHO DO POLIGONO, POIS TEM UMA FORMA DIFERENTE DE SER DESENHADO
-        global linha_poligono, linhas_poligono
         if self.view.obter_forma() != "Polígono":
             return
-        else:
-            if self.view.obter_forma()() == "Polígono" and len(pontos_poligono) >= 2:
-                if linha_poligono is not None:    
-                    self.view.canvas.delete(linha_poligono)
             
-                x_base = pontos_poligono[-2]
-                y_base = pontos_poligono[-1]
-                x_atual = event.x
-                y_atual = event.y
+        # DESENHA LINHA QUE SEGUE O MOUSE
+        if len(self.desenho.pontos_poligono) >= 2:
+            if self.linha_poligono_temp is not None:    
+                self.view.canvas.delete(self.linha_poligono_temp)
+        
+            x_base = self.desenho.pontos_poligono[-2]
+            y_base = self.desenho.pontos_poligono[-1]
+            x_atual = event.x
+            y_atual = event.y
 
-                linha_poligono = self.view.canvas.create_line(x_base, y_base, x_atual, y_atual, dash=(4,2))
+            self.linha_poligono_temp = self.view.canvas.create_line(x_base, y_base, x_atual, y_atual, dash=(4,2))
             
-            else:
-                return
-            
-    
-    def terminarDesenho(self, event):
-        global figura_preview, linhas_poligono
-        if self.view.obter_forma() == "Polígono":  #CHECAGEM PRA NÃO ATRAPALHAR A CRIAÇÃO DO POLIGONO QUANDO FOR DADO APENAS UM CLICK NO MOUSE
-            return
-        else:
-            if figura_preview:
-            # GUARDANDO NA LISTA AS FIGURAS AO SOLTAR O MOUAW
-                linhas_poligono = []
-                self.desenho.adicionar_figura(figura_preview)
-                figura_preview = None  
-                self.desenhar_tudo()
-
-    def terminar_poligono(self, cor_borda, cor_preenchimento):
-        global figura_preview, linhas_poligono
+    def terminar_desenho(self, event):
         if self.view.obter_forma() == "Polígono":
-            self.pontos_poligono.pop()
+            return
             
-            cor_borda = self.view.obter_cor_borda()
-            cor_preenchimento = self.view.obter_cor_preench()
+        if self.desenho.figura_preview:
+            # SALVA NO BANCO DE DADOS DO MODELO
+            self.desenho.adicionar_figura(self.desenho.figura_preview)
+            self.desenho.reiniciar_preview()  
+            self.desenhar_tudo()
 
-            figura_preview = Polígono(pontos_poligono, cor_borda, cor_preenchimento)
-            for linha in linhas_poligono:
-            canvas.delete(linha)  #APAGA AS LINHAS DE RASCUNHO
+    def terminar_poligono(self, event):
+        if self.view.obter_forma() != "Polígono":
+            return
+            
+        cor_borda = self.view.obter_cor_borda()
+        cor_preenchimento = self.view.obter_cor_preench()
 
-        canvas.delete(linha_poligono)   #APAGA A ULTIMA LINHA
-        linha_poligono = None
-        linhas_poligono =[]    #LIMPA A LISTA DE LINHAS
+        # NO MODELO DESENHOS HÁ UMA FUNÇÃO QUE LIMPA O CLIQUE DUPLO, AQUI ESTÁ SENDO USADA ELA
+        self.desenho.finalizar_poligono(cor_borda, cor_preenchimento)
 
-        figuras.append(figura_preview) #GUARDANDO O POLIGONO NA LISTA DE FIGURAS E DEPOIS DESENHANDO ELE
-        figura_preview = None
-        desenhar_tudo()
+        # APAGA AS LINAHS PRETAS E TEMPORÁRIAS
+        for linha in self.linhas_poligono_fixas_temp:
+            self.view.canvas.delete(linha)  
+        if self.linha_poligono_temp:
+            self.view.canvas.delete(self.linha_poligono_temp)
 
-        pontos_poligono = [] #LIMPANDO A LISTA DE PONTOS PARA FICAR LIMPA PARA O PROXIMO POLIGONO
-    else:
-        return
-    
+        # RESETA OS RASCUNHOS
+        self.linha_poligono_temp = None
+        self.linhas_poligono_fixas_temp = []
+
+        # REFAZ A TELA COM O NOVO POLÍGONO VINDO DO MODELO
+        self.desenhar_tudo()
+
     def desenhar_tudo(self):
         # LIMPA O CANVAS
         self.view.canvas.delete("all")
         
         # DESENHA TODAS AS FIGURAS CONCLUÍDAS
         for figura in self.desenho.figuras:
-            figura.desenhar(self.view.canvas)
+            desenhar_figura_na_tela(self.view.canvas, figura, rascunho=False)
         
         # DESENHA O RASCUNHO ATUAL, SE HOUVER
-        if figura_preview:
-            figura_preview.desenhar(self.view.canvas, rascunho=True)
+        if self.desenho.figura_preview:
+            desenhar_figura_na_tela(self.view.canvas, self.desenho.figura_preview, rascunho=True)
 
     def iniciar(self):
         self.view.janela.mainloop()
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/Ambiente de desenhos/view/view.py
+++ src/Ambiente de desenhos/view/view.py
@@ -1,35 +1,45 @@
+
 from tkinter import *
 from tkinter import colorchooser
 
-#CLASSE GERAL DO VIEW
+
+def desenhar_figura_na_tela(canvas, figura, rascunho=False):
+    figura.desenhar(canvas, rascunho)
+
+# CLASSE GERAL DO VIEW
 class View:
     def __init__(self, janela_principal):
-        #CRIAR JANELA GERAL E A ABA DE FUNCIONALIDADES
+        #---- CRIAR JANELA GERAL E A ABA DE FUNCIONALIDADES
         self.janela = janela_principal
         self.funcionalidades = Frame(janela_principal)
         self.funcionalidades.pack(pady=10)
-        #CRIAR O BOTÃO DE ESCOLHER FORMA
+
+        #----- CRIAR O BOTÃO DE ESCOLHER FORMA
         Label(self.funcionalidades, text="Escolha a Forma:").grid(row=0, column=0, sticky=E, padx=5, pady=5)
         self.forma_var = StringVar(janela_principal)
         self.forma_var.set("Retângulo")
         self.menu_formas = OptionMenu(self.funcionalidades, self.forma_var, "Retângulo", "Oval", "Círculo", "Linha", "Rabisco", "Polígono")
         self.menu_formas.grid(row=0, column=1, sticky=W, padx=(5,15), pady=5)
-        #CRIAR BOTAO DE ESCOLHER COR DA BORDA
+
+        #------ CRIAR BOTAO DE ESCOLHER COR DA BORDA
         Label(self.funcionalidades, text="Cor da Borda:").grid(row=0, column=2, sticky=E, padx=(15,5), pady=5)
         self.borda_var = StringVar(janela_principal)
         self.borda_var.set("black")
         self.BotaoBorda = Button(self.funcionalidades, text = "Selecionar Cor da Borda", command= lambda: self.escolher_cor(self.borda_var))
         self.BotaoBorda.grid(row = 0, column= 3, sticky= W, padx= (5,15), pady= 5)
-        #CRIAR BOTÃO DE ESCOLHER A COR DE PREENCHIMENTO
+
+        #------- CRIAR BOTÃO DE ESCOLHER A COR DE PREENCHIMENTO
         Label(self.funcionalidades, text="Cor de Preenchimento:").grid(row=0, column=4, sticky=E, padx=(15,5), pady=5)
         self.preencher_var = StringVar(janela_principal)
         self.preencher_var.set("white")
         self.BotaoPreench = Button(self.funcionalidades, text = "Selecionar cor do preenchimento", command= lambda: self.escolher_cor(self.preencher_var))
         self.BotaoPreench.grid(row = 0, column= 5, sticky= W, padx= (5,15), pady= 5)
-        #CRIAR BOTÃO DE APAGAR
-        #self.BotaoApagar = Button(self.funcionalidades, text="Apagar Tudo", command = funcao_apagar, fg="red")
-        #self.BotaoApagar.grid(row=0, column=6, sticky=W, padx=15, pady=5)
-        #CRIAR O CANVAS
+
+        #-------- CRIAR BOTÃO DE APAGAR
+        self.BotaoApagar = Button(self.funcionalidades, text="Apagar Tudo", fg="red")
+        self.BotaoApagar.grid(row=0, column=6, sticky=W, padx=15, pady=5)
+
+        #-------- CRIAR O CANVAS
         self.canvas = Canvas(janela_principal, bg="white", width=1920, height=1000)
         self.canvas.pack()
 
@@ -38,6 +48,7 @@ class View:
         cor_selecionada = colorchooser.askcolor()[1]
         if cor_selecionada:
             variavel_cor.set(cor_selecionada)
+
     #METODOS PARA OBTER AS VARIAVEIS DE FORMA E CORES
     def obter_forma(self):
         return self.forma_var.get()
